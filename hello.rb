29600c89d1caf0e0895bb0a000a12e5c0c2d4a21
shiba-x86ken <38150179+shiba-x86ken@users.noreply.github.com>
2018-04-10 09:55:11 +0900

    Add files via upload

---
+++ hello.rb
@@ -0,0 +1,28 @@
+=begin
+comment
+comment
+=end
+
+print "hello world."
+puts "hello world."; #+改行
+p "hello world."; #デバッグ用
+
+puts "--------------------------------------"
+
+#変数
+#英大文字 or _ で始める
+msg = "hello world."
+puts msg
+
+msg = "hello world again."
+puts msg
+
+#定数
+#英大文字 で始める
+VERSION = 1.1
+puts VERSION
+
+=begin
+VERSION = 1.2 #Erro
+puts VERSION
+=end
