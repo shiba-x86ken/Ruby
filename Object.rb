9880fb645cdc3487799f6f02f437c5dc5bbcaf37
shiba-x86ken <38150179+shiba-x86ken@users.noreply.github.com>
2018-04-11 11:01:17 +0900

    Add files via upload

---
+++ Object.rb
@@ -0,0 +1,17 @@
+#String Class
+puts "hello world.".length #文字数を返すメソッド.length
+puts "hello world.".reverse#文字列を反転されるメソッド.reverse
+
+msg = "hello world."
+puts msg.length
+puts msg.reverse
+
+puts "--------------------------------------"
+
+#Float Class （小数点メソッド）
+puts 1.2.round
+puts 1.2.floor
+
+VERSION = 1.2
+puts VERSION.round
+puts VERSION.floor
