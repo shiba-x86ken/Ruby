f1a1a690ad14cf8b8b8014a2f477f3df278f09ef
shiba-x86ken <38150179+shiba-x86ken@users.noreply.github.com>
2018-04-16 18:13:33 +0900

    Add files via upload

---
+++ %_Notation.rb
@@ -0,0 +1,15 @@
+#%
+
+puts "he\"llo"
+puts 'he\'llo'
+
+puts %(he"llo)
+puts %q(he'llo)
+
+puts "--------------------------------------"
+
+p ["red", "blue"]
+p ['red', 'blue']
+
+p %W(red blue)
+p %w(red blue)
