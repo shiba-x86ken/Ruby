fd0c7a65cfb48bd5b20b31217bf0e7ed005b3689
shiba-x86ken <38150179+shiba-x86ken@users.noreply.github.com>
2018-04-18 11:31:09 +0900

    Add files via upload

---
+++ for_Statement.rb
@@ -0,0 +1,31 @@
+#for
+for i in 15..20 do #15Ç©ÇÁ20Ç‹Ç≈åJÇËë÷Ç¶ÇµÇ¬Ç¬ÅCiÇ…äiî[Ç∑ÇÈ
+  p i
+end
+puts
+
+for colors in ["red", "blue"]
+ p colors
+end
+
+puts
+for name, score in {taguchi:200, koji:600}
+ p "#{name}:#{score}"
+end
+
+puts "--------------------------------------"
+
+#each
+(15..20).each do |i|
+  p i
+end
+puts
+
+["red", "blue"].each do |colors|
+ p colors
+end
+
+puts
+{taguchi:200, koji:600}.each do |name, score|
+ p "#{name}:#{score}"
+end
